f623335be76d4ba61cb0377cf70a4890e32f6d4c
Braydan C <braydanc@gmail.com>
2022-06-20 15:26:28 -0500

    Update kaliinstalls.sh

--- kaliinstalls.sh
+++ kaliinstalls.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
 
 #update & upgrade kali
-echo -e "Updating Kali"
+echo "Updating Kali"
 sudo apt-get update && sudo apt-get upgrade
 
 #download apps/tools
-echo -e "Tools will be downloaded in /opt/ folder"
-echo -e "Downloading guest installations"
+echo "Tools will be downloaded in /opt/ folder"
+echo "Downloading guest installations"
 sudo apt install -y --reinstall virtualbox-guest-x11
 
-echo -e "Downloading pimpmykali"
+echo "Downloading pimpmykali"
 wget https://github.com/Dewalt-arch/pimpmykali/blob/be57a43127a53355454bb90033e6dec4d9075825/pimpmykali.sh -P /opt/
 
-echo -e "Installing stegseek"
+echo "Installing stegseek"
 sudo apt-get install ./stegseek_0.6-1.deb
 
-echo -e "Installing gobuster"
+echo "Installing gobuster"
 sudo apt-get install -v https://github.com/OJ/gobuster/v3@latest
 
-echo -e "Installing pimpmykali"
+echo "Installing pimpmykali"
 ./opt/pimpmykali/pimpmykali.sh
 
-echo -e "rebooting"
+echo "rebooting"
 sudo reboot -f
